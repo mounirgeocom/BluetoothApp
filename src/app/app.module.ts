f429936d36a76f4f079736fb035b42ba324d5fa9
mounir Baghdad Jamai <mounirbaghdadjamai@MacBook-Pro-de-mounir.local>
2022-09-03 22:08:08 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BluetoothClassicSerialPort } from '@awesome-cordova-plugins/bluetooth-classic-serial-port/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    BluetoothClassicSerialPort
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BluetoothClassicSerialPort } from '@awesome-cordova-plugins/bluetooth-classic-serial-port/ngx';
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,42 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  pairedList = [];
+  listToggle= false;
+
+  constructor(
+    public navCtrl: NavController,
+    private alertCtrl: AlertController,
+    private bluetoothSerial: BluetoothClassicSerialPort,) {
+    this.checkBluetoothEnabled();
+  }
+
+  checkBluetoothEnabled() {
+    this.bluetoothSerial.isEnabled().then(success => {
+      this.listPairedDevices();
+    }, error => {
+      this.showError(` Please Enable Bluetooth `);
+    });
+  }
+
+  listPairedDevices() {
+    this.bluetoothSerial.list().then(success => {
+      this.pairedList = success;
+      this.listToggle = true;
+    }, error => {
+      this.showError(`Please Enable Bluetooth`);
+      this.listToggle = false;
+    });
+  }
+
+
+  async showError(error:string) {
+    const myAlert = this.alertCtrl.create({
+      header: 'Error',
+      subHeader: error
+    });
+
+   await (await myAlert).present();
+  }
 
 }
